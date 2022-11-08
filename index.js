2a3fac0828474650c6883af6af40f78cf745c810
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-11-08 19:10:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ var indexFile = require('./videos.json');
     const t4 = performance.now();
     customSearch.forEach(async e => {
         try {
-        const info = await search.search(e);
+        const info = await search.search(e, { limit: 5 });
         info.forEach(async (video) => {
             try {
             if(videos.includes(`${video.id}.mp4`)) return;
